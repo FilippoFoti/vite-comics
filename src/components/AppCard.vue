049db2017e37ea03ec3808fef1049bbdd6134521
FilippoFoti <filippofoti7@gmail.com>
2023-04-14 15:42:17 +0200

    Main Completed

---
+++ src/components/AppCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: "AppCard",
+    props: {
+        thumb: String,
+        series: String
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <figure>
+            <img :src="thumb" alt="Copertina">
+        </figure>
+        <h2>{{ series }}</h2>
+    </div>
+
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
+
+.card {
+    width: calc(100% / 6 - 20px);
+
+    figure {
+        height: 200px;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    
+    }
+    h2 {
+        padding-top: 10px;
+        font-size: 0.9rem;
+        text-align:start;
+        color: white;
+        font-weight: 400;
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,87 @@
 <script>
+import AppCard from "./AppCard.vue";
+
 export default {
+    components: {
+        AppCard
+    },
     name: "AppMain",
     data() {
         return {
+            comicArray: [
+                {
+                    thumb: 'https://www.coverbrowser.com/image/action-comics/1-1.jpg',
+                    price: '$19.99',
+                    series: 'Action Comics',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg',
+                    price: '$3.99',
+                    series: 'American Vampire 1976',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg',
+                    price: '$16.99',
+                    series: 'Aquaman',
+                    type: 'graphic novel',
+                },
+                {
+                    thumb: 'https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg',
+                    price: '$2.99',
+                    series: 'Batgirl',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg',
+                    price: '$3.99',
+                    series: 'Batman',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg',
+                    price: '$2.99',
+                    series: 'Batman Beyond',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg',
+                    price: '$3.99',
+                    series: 'Batman/Superman',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg',
+                    price: '$4.99',
+                    series: 'Batman/Superman Annual',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg',
+                    price: '$5.99',
+                    series: 'Batman: The Joker War Zone',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg',
+                    price: '$6.99',
+                    series: 'Batman: Three Jokers',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg',
+                    price: '$4.99',
+                    series: 'Batman: White Knight Presents: Harley Quinn',
+                    type: 'comic book',
+                },
+                {
+                    thumb: 'https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg',
+                    price: '$16.99',
+                    series: 'Catwoman',
+                    type: 'graphic novel',
+                },
+            ],
             features: [
                 {
                     img: "buy-comics-digital-comics.png",
@@ -43,15 +122,19 @@ export default {
 <template>
     <main>
         <div class="top">
-            <div class="content">
-                <h2> --> Content goes here &lt;-- </h2>
+            <div class="content-top">
+                <button>CURRENT SERIES</button>
+            </div>
+            <div class="content-bottom">
+                <AppCard v-for="(elem, index) in comicArray" :key="index" :thumb="elem.thumb" :series="elem.series"/>
+                <button>LOAD MORE</button>
             </div>
         </div>
         <div class="bottom">
             <div class="features">
                 <ul>
-                    <li v-for="image in features">
-                        <a :href="image.url">
+                    <li v-for="img in features">
+                        <a :href="img.url">
                             <img :src="getImagePath(img)" alt="Logo">
                             <p>{{ img.name }}</p>
                         </a>
@@ -67,18 +150,45 @@ export default {
 @use "../style/partials/variables" as *;
 
 .top {
-    background-color: black;
-    height: 130px;
+    background-color: rgba(28, 28, 28, 1);
+    width: 100%;
 
-    .content {
-        width: 70%;
-        height: 130px;
-        margin: 0 auto;
-        @include flex(center, start, row);
+    .content-top {
+        height: 300px;
+        background-image: url(../assets/img/jumbotron.jpg);
+        background-size: cover;
+        position: relative;
 
-        h2 {
-            font-size: 1.7rem;
+        button {
             color: white;
+            font-size: 1.3rem;
+            font-weight: 700;
+            padding: 12px 12px;
+            background-color: $primary;
+            border: 1px solid $primary;
+            position: absolute;
+            bottom: -25px;
+            left: 230px;
+        }
+    }
+
+    .content-bottom {
+        padding: 40px 0 20px 0;
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 20px;
+
+        button {
+            color: white;
+            font-size: 0.7rem;
+            font-weight: 700;
+            padding: 7px 40px;
+            background-color: $primary;
+            border: 1px solid $primary;
+            cursor: pointer;
         }
     }
 }
